5fb61aed55fae217d4e8808cdb327d42a16de3bd
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-03-10 21:14:09 +0900

    Fully implement game logic

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
+const displayController = (() => {
+  const renderMesssage = (message) => {
+    document.querySelector("#message").innerHTML = message;
+  }
+  return {
+    renderMesssage,
+  }
+})();
+
 const Gameboard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""]
 
@@ -54,18 +63,33 @@ const Game = (() => {
   }
 
   const handleClick = (event) => {
+    if (gameOver) {
+      return;
+    }
     let index = parseInt(event.target.id.split("-")[1]);
     if (Gameboard.getGameboard() [index] !== "")
       return;
+
     Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+      gameOver = true;
+      displayController.renderMesssage(`${players[currentPlayerIndex].name} wins`)
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMesssage("It's a tie!")
+    }
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   }
+
 
   const restart = () => {
     for (let i = 0; i < 9; i++) {
       Gameboard.update(i, "");
     }
     Gameboard.render();
+    gameOver = false;
+    document.querySelector("#message").innerHTML = "";
   }
 
   return {
@@ -74,6 +98,31 @@ const Game = (() => {
     handleClick
   }
 })();
+
+function checkForWin(board) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i=0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]){
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForTie(board) {
+  return board.every(cell => cell !== "")
+}
+
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
